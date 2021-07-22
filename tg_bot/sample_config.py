2494705eb2eca5dbe50d721b800a9393c1cfb173
sunaif-adkar <86029415+sunaif-adkar@users.noreply.github.com>
2021-07-22 11:38:46 +0530

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -1,6 +1,6 @@
 if not __name__.endswith("sample_config"):
     import sys
-    print("The README is there to be read. Extend this sample config to a config file, don't just rename and change "
+    print("The README is there to be read. Extend this sample config to a config file, don't just rename it and change "
           "values here. Doing that WILL backfire on you.\nBot quitting.", file=sys.stderr)
     quit(1)
 
