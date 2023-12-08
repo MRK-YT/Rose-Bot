2b6b48df78b85cd584cfdcaf006c9f4fe238c152
Onix-79 <63454564+Onix-79@users.noreply.github.com>
2023-12-08 07:53:10 +0100

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -2,9 +2,9 @@ class Config(object):
     LOGGER = True
 
     # REQUIRED
-    API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "YOUR USERNAME HERE"
+    API_KEY = "6952640033:AAGbjWZ63FTW1zvk-s0rfSIZhbDqpN0jz7E"
+    OWNER_ID = "1486985072"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "Gentoo79"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
@@ -15,9 +15,9 @@ class Config(object):
     URL = None
 
     # OPTIONAL
-    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
-    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    SUDO_USERS = [1486985072]  # List of id's (not usernames) for users which have sudo access to the bot.
+    SUPPORT_USERS = [1486985072]  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = [1486985072]  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
     DONATION_LINK = None  # EG, paypal
     CERT_PATH = None
     PORT = 5000
