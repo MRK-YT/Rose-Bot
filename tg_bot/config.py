b38bf8c96508a9f5b4f75101182208986310520d
Onix-79 <63454564+Onix-79@users.noreply.github.com>
2023-12-08 07:48:28 +0100

    Create config.py

---
+++ tg_bot/config.py
@@ -0,0 +1,36 @@
+class Config(object):
+    LOGGER = True
+
+    # REQUIRED
+    API_KEY = "YOUR KEY HERE"
+    OWNER_ID = "YOUR ID HERE"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "YOUR USERNAME HERE"
+
+    # RECOMMENDED
+    SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
+    MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
+    LOAD = []
+    NO_LOAD = ['translation', 'rss']
+    WEBHOOK = False
+    URL = None
+
+    # OPTIONAL
+    SUDO_USERS = []  # List of id's (not usernames) for users which have sudo access to the bot.
+    SUPPORT_USERS = []  # List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    WHITELIST_USERS = []  # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    DONATION_LINK = None  # EG, paypal
+    CERT_PATH = None
+    PORT = 5000
+    DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
+    STRICT_GBAN = False
+    WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
+    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
+    ALLOW_EXCL = False  # Allow ! commands as well as /
+    BMERNU_SCUT_SRELFTI = 0
+
+class Production(Config):
+    LOGGER = False
+
+
+class Development(Config):
+    LOGGER = True
